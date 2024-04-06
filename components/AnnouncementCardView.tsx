c2cd8f6502759fc35f2431cb37bfd119a6ba1d11
Jinghao1209 <84082781+Jinghao1209@users.noreply.github.com>
2024-04-06 16:37:36 +0800

    fix(AnnouncementCardView): empty space problem

--- components/AnnouncementCardView.tsx
+++ components/AnnouncementCardView.tsx
@@ -13,23 +13,28 @@ export default function AnnouncementCardView({
   time: number;
 }) {
   const handleTime = (time: number) => {
-    return "7小时前";
+    return '7小时前';
   };
   return (
     <View className="bg-[#222] rounded-2xl px-5 py-3">
       {/* title */}
       <Text className="font-semibold text-xl">{handleText(title, 25)}</Text>
       {/* content */}
-      <Text className="pb-4 pt-0" style={{fontSize:12}}>{handleText(content, 100)}</Text>
+      <Text className="pb-4 pt-0" style={{ fontSize: 12 }}>
+        {handleText(content, 100)}
+      </Text>
       {/* tags */}
-      <View className='bg-[#222]'>
-        <Text className='font-medium'>Important Announcements</Text> {/* TODO: should i need this? */}
+      <View className="bg-[#222]">
+        {/* TODO: should i need this? */}
+        <Text className="font-medium">Important Announcements</Text>
       </View>
       {/* post info */}
       <View className="bg-[#222] flex-row justify-between">
         {/* author */}
         <View className="bg-[#222]">
-          <Text className="font-bold text-gray-300">{handleText(authorName, 25)}</Text>
+          <Text className="font-bold text-gray-300">
+            {handleText(authorName, 25)}
+          </Text>
         </View>
         {/* time */}
         <Text className="font-bold">{handleTime(time)}</Text>
