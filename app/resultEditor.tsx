3ba706c1d217d87f3f27313a4348930e563f324a
Jinghao1209 <84082781+Jinghao1209@users.noreply.github.com>
2024-06-22 21:48:04 +0800

    feat(resultEditor.tsx): import result

--- app/resultEditor.tsx
+++ app/resultEditor.tsx
@@ -1,12 +1,14 @@
+import * as Clipboard from 'expo-clipboard';
+import { useNavigation, useRouter } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Alert, Pressable, TouchableOpacity } from 'react-native';
 
 import { ScrollView, Text, TextInput, View } from '@/components/Themed';
 import Colors from '@/constants/Colors';
 import useResultStore, { subjects } from '@/store/useResultStore';
-import { useRouter } from 'expo-router';
 
 export default function ResultEditorScreen() {
+  const navigation = useNavigation();
   const router = useRouter();
   const resultStore = useResultStore((state) => state);
   const [result, setResult] = useState<{ [key: string]: number | string }>(
@@ -18,11 +20,14 @@ export default function ResultEditorScreen() {
     //   // console.log({ ...res, [code]: Number(mark) })
     //   return { ...res, [code]: Number(mark) };
     // });
-    setResult((res) => ({ ...res, [code]: Number(mark) }));
+    setResult((res) => ({ ...res, [code]: mark }));
   };
 
-  const resultComplete = () => {
-    let obj: (keyof typeof subjects)[] = [];
+  const resultComplete = (r = result) => {
+    let obj: (keyof typeof subjects)[] = [],
+      result = r; // garbage, absolutely rubbish code, wtf is this??!
+
+    console.log("resultEditor", result);
 
     for (let k in result) {
       if (Number(result[k]) >= 0 && Number(result[k]) <= 100) {
@@ -55,6 +60,43 @@ export default function ResultEditorScreen() {
     router.back();
   };
 
+  const fetchCopiedText = async () => {
+    const text = await Clipboard.getStringAsync();
+    try {
+      let json = JSON.parse(text);
+      // 有bug，懒惰修，不然我也不想要使用这个的啊啊啊啊啊啊啊啊啊！！！！！
+      resultComplete(json);
+    } catch (e) {
+      Alert.alert(
+        'failed with error',
+        e instanceof SyntaxError ? e.message : 'Unknown Error'
+      );
+    }
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Pressable onPress={fetchCopiedText}>
+          {({ pressed }) => (
+            <Text
+              style={{
+                marginRight: 10,
+                opacity: pressed ? 0.5 : 1,
+                color: Colors.ios.linkBlue,
+                paddingHorizontal: 8,
+                paddingVertical: 5,
+                // backgroundColor: 'white',
+              }}
+            >
+              导入
+            </Text>
+          )}
+        </Pressable>
+      ),
+    });
+  }, []);
+
   return (
     <ScrollView className="flex-1">
       <View className="flex-1 justify-center">
@@ -80,8 +122,11 @@ export default function ResultEditorScreen() {
             </View>
           ))}
         </View>
-        <View className="text-center items-center m-5">
-          <TouchableOpacity className="px-4 py-2" onPress={resultComplete}>
+        <View className="text-center items-center justify-center mt-0">
+          <TouchableOpacity
+            className="px-4 py-2"
+            onPress={() => resultComplete()}
+          >
             <Text
               className="font-bold text-xl"
               style={{ color: Colors.ios.linkBlue }}
